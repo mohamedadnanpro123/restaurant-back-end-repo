2d1e2591e8054f27fc082e38bafa8edd56976ab9
Mohamed Adnan <mohamed_adnan_ali@hotmail.com>
2025-12-05 00:40:57 +0200

    Fix PM2 process name in deployment script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/backend
-
 # Delete existing PM2 process
-pm2 delete restaurant-api-dev || true
-
+pm2 delete restaurant-api-development || true
 # Start the application
-pm2 start server.js --name restaurant-api-dev
-
+pm2 start server.js --name restaurant-api-development
 # Save PM2 configuration
 pm2 save
